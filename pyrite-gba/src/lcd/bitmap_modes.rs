333dbbfb977cb08fe0c8645f4f3e2e3d91e89a14
Adolph C <adolphc@outlook.com>
2019-10-22 15:40:58 -0400

    Implemented mosaic for bitmap modes 3 and 4.

--- pyrite-gba/src/lcd/bitmap_modes.rs
+++ pyrite-gba/src/lcd/bitmap_modes.rs
@@ -3,6 +3,7 @@
 //! only 16KBytes of VRAM can be used for OBJ tiles.
 
 use super::Line;
+use super::effects::apply_mosaic;
 use super::super::GbaMemory;
 use super::super::memory::read16_le;
 
@@ -20,20 +21,36 @@ use super::super::memory::read16_le;
 /// The background occupies 75 KBytes (06000000-06012BFF), most of the 80 Kbytes BG area,
 /// not allowing to redraw an invisible second frame in background, so this mode is mostly recommended for still images only.
 pub fn mode3(line: u32, out: &mut Line, memory: &mut GbaMemory) {
-    let pixel_data_start = 480 * line as usize;
-    for pixel_offset in 0..240 {
-        let pixel = read16_le(&memory.mem_vram, pixel_data_start + (pixel_offset * 2)) | 0x8000;
-        out[pixel_offset as usize] = pixel;
+    // Bitmap Modes use BG2
+
+    if memory.ioregs.dispcnt.screen_display_bg2() {
+        let mosaic_x = if memory.ioregs.bg_cnt[2].mosaic() { memory.ioregs.mosaic.bg_h_size() as u32 + 1 } else { 0 };
+        let mosaic_y = if memory.ioregs.bg_cnt[2].mosaic() { memory.ioregs.mosaic.bg_v_size() as u32 + 1 } else { 0 };
+
+        let y = apply_mosaic(line, mosaic_y);
+        let pixel_data_start = 480 * y as usize;
+        for screen_x in 0..240 {
+            let pixel_offset = apply_mosaic(screen_x, mosaic_x) as usize;
+            let pixel = read16_le(&memory.mem_vram, pixel_data_start + (pixel_offset * 2)) | 0x8000;
+            out[screen_x as usize] = pixel;
+        }
     }
 }
 
 pub fn mode4(line: u32, out: &mut Line, memory: &mut GbaMemory) {
     const FRAME1_OFFSET: usize = 0xA000;
 
-    let pixel_data_start = 240*(line as usize) + FRAME1_OFFSET*(memory.ioregs.dispcnt.frame() as usize);
-    for pixel_offset in 0..240 {
-        let pixel = memory.mem_vram[pixel_data_start + pixel_offset];
-        out[pixel_offset as usize] = memory.palette.get_bg256(pixel);
+    if memory.ioregs.dispcnt.screen_display_bg2() {
+        let mosaic_x = if memory.ioregs.bg_cnt[2].mosaic() { memory.ioregs.mosaic.bg_h_size() as u32 + 1 } else { 0 };
+        let mosaic_y = if memory.ioregs.bg_cnt[2].mosaic() { memory.ioregs.mosaic.bg_v_size() as u32 + 1 } else { 0 };
+
+        let y = apply_mosaic(line, mosaic_y) as usize;
+        let pixel_data_start = 240*y + FRAME1_OFFSET*(memory.ioregs.dispcnt.frame() as usize);
+        for screen_x in 0..240 {
+            let pixel_offset = apply_mosaic(screen_x, mosaic_x) as usize;
+            let pixel = memory.mem_vram[pixel_data_start + pixel_offset];
+            out[screen_x as usize] = memory.palette.get_bg256(pixel);
+        }
     }
 }
 
