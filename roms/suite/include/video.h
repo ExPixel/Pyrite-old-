cdc530408b9025b864d43fcb231b881a3a6adff2
Adolph C <adolphc@outlook.com>
2020-01-20 15:14:13 -0500

    Removed old mgba roms.

--- roms/suite/include/video.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "common.h"
-#include "suite.h"
-
-extern const struct TestSuite videoTestSuite;
--- roms/suite/src/tests/basic.s
+++
@@ -1,5 +0,0 @@
-#include "macros.s"
-
-TEST_ALL(calibrate, , ,)
-TEST_ALL(testNop, nop, ,)
-TEST_ALL(testNop2, nop ; nop, ,)
